e7dd7ad92f26eb947a6b78945be6d3c735833415
Hitesh Sharma <jugardemailhai@gmail.com>
2022-06-11 10:49:04 +0530

    Added LoginDemo.feature

--- src/test/java/com/kits/steps/GoogleDemo.java
+++ src/test/java/com/kits/steps/GoogleDemo.java
@@ -1,35 +1,48 @@
 package com.kits.steps;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.kits.util.AbstractChromeWebDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class GoogleDemo {
+public class GoogleDemo extends AbstractChromeWebDriver {
 
 	@Given("browser is open")
 	public void browser_is_open() {
-		System.out.println("open chrome");
+		beforeTest();
 	}
 
 	@Given("user is on google page")
 	public void user_is_on_google_page() {
 		System.out.println("Go to google page");
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
 	}
 
 	@When("user enters text in search bar")
 	public void user_enters_text_in_search_bar() {
-	   System.out.println("enter text in search bar");
+		System.out.println("enter text in search bar");
+		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.sendKeys("Java");
+		sleep(1);
 	}
 
 	@When("clicks on search button")
 	public void clicks_on_search_button() {
-	    System.out.println("clicks on search button");
+		System.out.println("clicks on search button");
+		WebElement element = driver.findElement(By.name("btnK"));
+		element.click();
 	}
 
 	@Then("user is navigated to search results page")
 	public void user_is_navigated_to_search_results_page() {
-	    System.out.println("verify serach result page is displayed..");
+		System.out.println("verify serach result page is displayed..");
+		sleep(3);
+		driver.quit();
 	}
 
 }
-
